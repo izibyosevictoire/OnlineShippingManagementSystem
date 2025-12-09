0b0a56baa046c5eb5f6eee7c5abda3be70aabfc3
izibyose victoire <luandavicky5@gmail.com>
2025-12-09 14:26:32 +0200

    feat: initialize Spring Boot 4.0.0 + Java 21 + PostgreSQL

---
+++ src/test/java/com/shipping/shipping_system/ShippingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shipping.shipping_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
